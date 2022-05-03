af8de7fb6983e0be9c47a0ef590520b8536707fc
GesMon23 <100744918+GesMon23@users.noreply.github.com>
2022-05-02 20:29:00 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//CONSTRUIR UN PROGRAMA QUE DESPLIEGUE LOS NÚMEROS DEL 20 AL 30.
+#include <iostream>
+int main(int argc, char** argv) {
+	for(int i=20;i<=30;i++){
+		printf("%d\n", i);
+	}
+	system("pause");
+}
